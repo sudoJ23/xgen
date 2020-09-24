b2f13c9ef8a1a17dab6735635ecb13668ab38b1b
J23 <j23@localhost.localdomain>
2020-09-24 19:32:58 +0800

    SMH

--- modules/commands.py
+++ modules/commands.py
@@ -4,11 +4,11 @@ class Commands():
 
     def restart():
         print("[ INFO ] RESTARTING BOT")
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
+        self.python = sys.executable
+        os.execl(self.python, self.python, * sys.argv)
         return
 
     def updateFromGit():
         os.system("git pull")
-        restart()
+        self.restart()
         return
