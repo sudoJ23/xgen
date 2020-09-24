dd56418e03425dc15aa9e5ce2addbb4fcaf24724
J23 <j23@localhost.localdomain>
2020-09-24 19:05:55 +0800

    SMH

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ configOpen = codecs.open('config.json', 'r', 'utf-8')
 config = json.load(configOpen)
 
 class Client(discord.Client):
+    def __init__():
+        msg = Message()
+
     async def on_ready(self):
         print("Logged on as {0}".format(self.user))
         return
 
-    async def on_message(message):
+    async def on_message(self, message):
         if config['bot']['printMessage'] == "true":
             print('Message from {0.guild.name} sender {0.author} : {0.content}'.format(message))
+            return
 
         if message.author == client.user:
             return
-        msg.checkMessage(message)
+
+        self.msg.checkMessage(message)
         return
 
 client = Client()
