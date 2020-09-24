4a6e7e9aca5a02a114a63932d3abbfdc7c2cdaca
J23 <j23@localhost.localdomain>
2020-09-24 20:24:21 +0800

    fix some error

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class Client(discord.Client):
 
     async def on_ready(self):
         print("Logged on as {0}".format(self.user))
-        self.msg = Message()
         return
 
     async def on_message(self, message):
@@ -18,7 +17,8 @@ class Client(discord.Client):
         if message.author == client.user:
             return
 
-        self.msg.checkMessage(message)
+        msg.checkMessage(message)
 
+msg = Message()
 client = Client()
 client.run(config['bot']['token'])
