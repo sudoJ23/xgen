3580a0dc4f6444232a0f8542e75f0631b3588079
J23 <j23@localhost.localdomain>
2020-09-25 02:13:32 +0800

    idk what happen

--- modules/commands.py
+++ modules/commands.py
@@ -11,10 +11,10 @@ class Commands():
         os.execl(python, python, * sys.argv)
 
     async def updateFromGit(self, message):
-        str(result, "utf-8")) = subprocess.check_output('git pull', shell=True)
-        if "Already" in result:
-            await message.channel.send(result)
+        result = subprocess.check_output('git pull', shell=True)
+        if "Already" in str(result, "utf-8")):
+            await message.channel.send(str(result, "utf-8"))
             return
         else:
-            await message.channel.send(result)
+            await message.channel.send(str(result, "utf-8"))
             self.restart()
