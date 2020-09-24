ba87017ef6989d7d147de0218c87bbb0d8a6946c
J23 <j23@localhost.localdomain>
2020-09-24 18:32:16 +0800

    do some changes

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
+from modules import *
 import discord, json, os, codecs, sys
 
 configOpen = codecs.open('config.json', 'r', 'utf-8')
 config = json.load(configOpen)
-
-class Client(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
-        if message.author == client.user:
-            return
-
-        if message.content.startswith('>restart'):
-            restart()
-        elif message.content.startswith('>updateGit'):
-            updateFromGit()
-
-def restart():
-    print("[ INFO ] RESTARTING BOT")
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
-
-def updateFromGit():
-    os.system("git pull")
-    restart()
 
 client = Client()
 client.run(config['bot']['token'])
